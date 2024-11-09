7700c7622224b5dbb5289a55b11c48bb8d6a9e06
danish2705 <danishmeraj06@gmail.com>
2024-11-09 12:58:54 +0530

    added packeges

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,11 @@
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/header/header";
 function App() {
-  return <di>Hello</di>;
+  return (
+    <Router>
+      <Header />
+    </Router>
+  );
 }
 export default App;
